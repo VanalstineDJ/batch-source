b2641425b8c775ff001f3e15acde55889fe8848b
David Van Alstine <VanalstineDJ@gmail.com>
2020-08-03 23:18:04 -0400

    getting data from a server

--- Angular/TourOfHeroes/angular-tour-of-heroes/src/app/hero.service.ts
+++ Angular/TourOfHeroes/angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,15 @@ export class HeroService {
     return of(HEROES.find(hero => hero.id === id));
   }
 
-  constructor(private messageService: MessageService) { }
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
+
+}
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+    private heroesUrl = 'api/heroes' /* URL to web api*/) { }
+    
 }
